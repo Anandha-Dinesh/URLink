d26678a4c283cbcbbe8bef76f5e0f2cf4c96e8ef
codemon003 <codemon57@gmail.com>
2024-08-25 21:56:56 +0530

    Added get current tab URL functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState,useEffect} from 'react';
 import './App.css';
 
 function App() {
+  // const qrgenerator:function = async (:String) => {
+    
+  // }
+  const [currentUrl, setCurrentUrl] = useState<string | null>(null);
+  useEffect(() => {
+    const getCurrentTabUrl = async () => {
+        try {
+            const queryOptions = { active: true, lastFocusedWindow: true };
+            const [tab] = await chrome.tabs.query(queryOptions);
+
+            if (tab && tab.url) {
+                setCurrentUrl(tab.url);
+            } else {
+                console.error('No active tab found.');
+                setCurrentUrl(null);
+            }
+        } catch (error) {
+            console.error('Error fetching tab URL:', error);
+        }
+    };
+
+    getCurrentTabUrl();
+}, []);
+  
+ 
   return (
-    <div className="App">
-      Hello World
-    </div>
-  );
+    <h1 className="text-3xl font-bold underline">
+      {currentUrl}
+    </h1>
+  )
 }
 
 export default App;
